c6d826864a99f1b6a349de34860ae055b78136d4
Robbie Sherman <rob.w.sherman@gmail.com>
2015-06-08 13:43:01 +0100

    Move php connection

--- php/displaySite.php
+++ php/displaySite.php
@@ -1,13 +1,6 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, 'dev');
-	// Check connection
-	if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		}
+
+	include "connection.php"; //Connect to Database
 
 	// Get the id of the page
 	$id = $_GET['id'];
@@ -25,7 +18,8 @@
 	`tic_mwp`,
 	`dnc_mw`,
 	`homes_powered`,
-	`carbon_saved_tones`
+	`carbon_saved_tones`,
+	`epc`
 	FROM `top_table`
 	WHERE `id` = '$id';"
 	);
@@ -33,16 +27,41 @@
 		$data['site'][] = $r;
 	};
 
+	// Get the epc info.
+	$data['epc'] = [];
+	$query = mysqli_query($conn, "SELECT top_table.id,
+		epc.epcName
+		from top_table
+		left join epc
+		on top_table.epc=epc.epcIndex
+		where id = $id;"
+		);
+	WHILE($r = mysqli_fetch_assoc($query)) {
+		$data['epc'][] = $r;
+	};
+
+	// Get the admin info.
+	$data['admin'] = [];
+	$query = mysqli_query($conn, "SELECT id,
+		dno,
+		mpan_export
+		from top_table
+		where id = $id;"
+		);
+	WHILE($r = mysqli_fetch_assoc($query)) {
+		$data['admin'][] = $r;
+	};
+
+
+
 	// Get number of inverters for side $id
 	$query = mysqli_query($conn,
-	"SELECT COUNT(DISTINCT(`inverter_number`)) AS `number`
-	FROM `inverter_site_generation_$id`;"
+		"SELECT COUNT(DISTINCT(`inverter_number`)) AS `number`
+		FROM `inverter_site_generation_$id`;"
 	);
 	WHILE($r = mysqli_fetch_assoc($query)) {
 		$numberOfInverter = $r['number'];
 	};
-
-
 
 	// Get site generation based on $id
 	$i = 1;
@@ -60,7 +79,7 @@
 			$data['generation']['generation_'.$i]['data'][] = $r;
 		};
 		$i++;
-	}
+	};
 
 	print json_encode($data, JSON_NUMERIC_CHECK);
 ?>
