61fd631e1ea0a8c793b49f5bb520f34e7dd0203b
Robbie Sherman <rob.w.sherman@gmail.com>
2015-06-23 12:06:03 +0100

    Move php connection to connection.php

--- php/connection.php
+++ php/connection.php
@@ -7,5 +7,6 @@
 	// Check connection
 	if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-	};
+	}
+  // echo "Connection successful";
 ?>
--- php/pickUp.php
+++ php/pickUp.php
@@ -1,18 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-// Create connection
-$conn = new mysqli($servername, $username, $password, 'dev');
-// Check connection
-if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-$data = [];
-$query = mysqli_query($conn, "select id, name, tic_mwp from top_table;");
-WHILE($r = mysqli_fetch_assoc($query)) {
-	$data[] = $r;
-};
-print json_encode($data, JSON_NUMERIC_CHECK);
+
+	include "connection.php"; //Connect to Database
+
+	$data = [];
+	$query = mysqli_query($conn, "select id, name, tic_mwp from top_table;");
+	WHILE($r = mysqli_fetch_assoc($query)) {
+		$data[] = $r;
+	};
+	print json_encode($data, JSON_NUMERIC_CHECK);
 ?>
