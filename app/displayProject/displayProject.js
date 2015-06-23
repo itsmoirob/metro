dd40c674108c2e030456875f4e11ed2529e3b898
Robbie Sherman <rob.w.sherman@gmail.com>
2015-06-23 12:06:28 +0100

    move all routes to routes.js

--- app/displayProject/displayProject.js
+++ app/displayProject/displayProject.js
@@ -6,62 +6,7 @@ angular.module('displayProject', [
   'highcharts-ng'
 ])
 
-.config(function($stateProvider){
-  $stateProvider
-    .state('parent.displaySummaryProject', {
-      url:'/site/summary/:siteResult',
-      views: {
-        'display@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySite.tmpl.html'
-        },
-        'other@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteList.tmpl.html'
-        }
-      }
-    })
-    .state('parent.displayGenerationProject', {
-      url:'/site/generation/:siteResult',
-      views: {
-        'display@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteGeneration.tmpl.html'
-        },
-        'other@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteList.tmpl.html'
-        }
-      }
-    })
-    .state('parent.displayFinanceProject', {
-      url:'/site/finance/:siteResult',
-      views: {
-        'display@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteFinance.tmpl.html'
-        },
-        'other@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteList.tmpl.html'
-        }
-      }
-    })
-    .state('parent.displayAdminProject', {
-      url:'/site/admin/:siteResult',
-      views: {
-        'display@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteAdmin.tmpl.html'
-        },
-        'other@': {
-          controller: 'DisplayCtrl',
-          templateUrl: 'pages/displaySiteList.tmpl.html'
-        }
-      }
-    })
-    ;
-})
+
 
 .controller('DisplayCtrl', ['$scope', '$stateParams', '$http', '$log', '$state', function($scope,$stateParams,$http,$log,$state){
 
